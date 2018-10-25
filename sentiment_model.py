cbfa7ef6ab04796ee3af49b2a9aac401df8bd366
Yao Chong Lim <yaochonl@andrew.cmu.edu>
2018-10-25 03:30:51 -0400

    save sentiment model

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -19,6 +22,15 @@ class SentimentModel(nn.Module):
         # x = F.tanh(self.out(x)) * 3
         # sentiment is [-3, 3] range
         return x.squeeze()
+
+def save_sentiment(path, model):
+    torch.save(model.state_dict(), os.path.join(path, 'senti.bin'))
+
+def load_sentiment(path, embedding_dim, hidden_dim, device):
+    model = SentimentModel(embedding_dim, hidden_dim)
+    model.load_state_dict(torch.load(path))
+    model = model.to(device)
+    return model
 
 def eval_sentiment(data, model, latents):
     n_samples = len(data.dataset)
@@ -42,16 +54,19 @@ def eval_sentiment(data, model, latents):
     y_test = torch.cat(y_test)
     predictions = torch.cat(predictions)
     # print(y_test.size())
-    full_loss(predictions, y_test)
+    return full_loss(predictions, y_test)
 
-def train_sentiment(args, data, model, latents, valid_niter=10):
+def train_sentiment(args, data, model, latents, valid_niter=10, verbose=False):
     n_epochs = args['n_sentiment_epochs']
     lr = args['sentiment_lr']
+    if verbose:
+        lr = 1e-6
 
     n_samples = len(data.dataset)
     loss_function = nn.L1Loss(reduce=False)
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
+    train_losses = []
     for i in range(n_epochs):
         epoch_loss = 0
         for j, senti in data:
@@ -60,25 +75,49 @@ def train_sentiment(args, data, model, latents, valid_niter=10):
             
             loss = loss_function(senti_predict, senti)
             epoch_loss += loss.sum()
+            #if verbose:
+            #    print(epoch_loss)
+            #    print(loss.mean())
 
             loss.mean().backward()
             optimizer.step()
 
+        train_losses.append(epoch_loss)
+        #if verbose:
+        #    sys.exit()
         if i % valid_niter == 0:
             print("Epoch {}: {}".format(i, epoch_loss / n_samples))
+    print("Epoch {}: {}".format(i, epoch_loss / n_samples))
+    return train_losses
 
-def train_sentiment_for_latents(args, latents, sentiment_data, device):
+def train_sentiment_for_latents(args, latents, sentiment_data, device, verbose=False,
+            model_save_path=None):
     hidden_dim = args['sentiment_hidden_size']
 
     embedding_dim = latents.size()[-1]
     senti_model = SentimentModel(embedding_dim, hidden_dim).to(device)
 
     print("Initial sentiment predictions")
-    eval_sentiment(sentiment_data, senti_model, latents)
+    senti_model.eval()
+    acc = eval_sentiment(sentiment_data, senti_model, latents)
+    if model_save_path is not None:
+        with open(os.path.join(model_save_path, 'acc_before.txt'), 'w') as f:
+            f.write(str(acc))
 
     print("Training sentiment model on sentence embeddings...")
-    train_sentiment(args, sentiment_data, senti_model, latents)
+    senti_model.train()
+    train_losses = train_sentiment(args, sentiment_data, senti_model, latents, verbose=verbose)
+
+    with open(os.path.join(model_save_path, 'senti_train_loss.txt'), 'w') as f:
+        for loss in train_losses:
+            f.write('{}\n'.format(loss))
+    if model_save_path is not None:
+        save_sentiment(model_save_path, senti_model)
 
     print("Sentiment predictions after training")
-    eval_sentiment(sentiment_data, senti_model, latents)
+    senti_model.eval()
+    acc = eval_sentiment(sentiment_data, senti_model, latents)
+    if model_save_path is not None:
+        with open(os.path.join(model_save_path, 'acc_after.txt'), 'w') as f:
+            f.write(str(acc))
     print("-----------------------------")
