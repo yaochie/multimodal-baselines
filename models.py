a5824ff7ffc055dd4a5e0bade4073872271f3e7d
Yao Chong Lim <yaochonl@andrew.cmu.edu>
2018-11-07 20:49:10 -0500

    simplify generator modules

--- models.py
+++ models.py
@@ -9,29 +9,32 @@ class AudioVisualGeneratorConcat(nn.Module):
         self.audio_embedding_dim = audio_embedding_dim
         self.visual_embedding_dim = visual_embedding_dim
 
-        self.embed2audio_mu = nn.Linear(self.audio_embedding_dim, audio_dim)
-        self.embed2audio_sigma = nn.Linear(self.audio_embedding_dim, audio_dim)
+        self.embed2audio = nn.ModuleDict({
+            'mu': nn.Linear(self.audio_embedding_dim, audio_dim),
+            'log_sigma': nn.Linear(self.audio_embedding_dim, audio_dim)
+        })
 
-        self.embed2visual_mu = nn.Linear(self.visual_embedding_dim, visual_dim)
-        self.embed2visual_sigma = nn.Linear(self.visual_embedding_dim, visual_dim)
+        self.embed2visual = nn.ModuleDict({
+            'mu': nn.Linear(self.visual_embedding_dim, visual_dim),
+            'log_sigma': nn.Linear(self.visual_embedding_dim, visual_dim)
+        })
 
     def freeze_weights(self):
         # freeze weights
-        for param in self.embed2audio_mu.parameters():
-            param.requires_grad = False
-        for param in self.embed2audio_sigma.parameters():
-            param.requires_grad = False
-        for param in self.embed2visual_mu.parameters():
-            param.requires_grad = False
-        for param in self.embed2visual_sigma.parameters():
-            param.requires_grad = False
+        for layer in self.embed2audio.values():
+            for param in layer.parameters():
+                param.requires_grad = False
+
+        for layer in self.embed2visual.values():
+            for param in layer.parameters():
+                param.requires_grad = False
 
     def forward(self, audio_embed, visual_embed):
-        audio_mu = self.embed2audio_mu(audio_embed)
-        audio_sigma = self.embed2audio_sigma(audio_embed)
+        audio_mu = self.embed2audio['mu'](audio_embed)
+        audio_sigma = self.embed2audio['log_sigma'](audio_embed).exp()
 
-        visual_mu = self.embed2visual_mu(visual_embed)
-        visual_sigma = self.embed2visual_sigma(visual_embed)
+        visual_mu = self.embed2visual['mu'](visual_embed)
+        visual_sigma = self.embed2visual['log_sigma'](visual_embed).exp()
 
         return (audio_mu, audio_sigma), (visual_mu, visual_sigma)
 
@@ -51,25 +54,28 @@ class AudioVisualGenerator(nn.Module):
         self.embedding = None
         self.embedding_dim = embedding_dim
 
-        self.embed2audio_mu = nn.Linear(self.embedding_dim, audio_dim)
-        self.embed2audio_sigma = nn.Linear(self.embedding_dim, audio_dim)
+        self.embed2audio = nn.ModuleDict({
+            'mu': nn.Linear(self.embedding_dim, audio_dim),
+            'log_sigma': nn.Linear(self.embedding_dim, audio_dim)
+        })
 
-        self.embed2visual_mu = nn.Linear(self.embedding_dim, visual_dim)
-        self.embed2visual_sigma = nn.Linear(self.embedding_dim, visual_dim)
+        self.embed2visual = nn.ModuleDict({
+            'mu': nn.Linear(self.embedding_dim, visual_dim),
+            'log_sigma': nn.Linear(self.embedding_dim, visual_dim)
+        })
 
         if frozen_weights:
             self.freeze_weights()
 
     def freeze_weights(self):
         # freeze weights
-        for param in self.embed2audio_mu.parameters():
-            param.requires_grad = False
-        for param in self.embed2audio_sigma.parameters():
-            param.requires_grad = False
-        for param in self.embed2visual_mu.parameters():
-            param.requires_grad = False
-        for param in self.embed2visual_sigma.parameters():
-            param.requires_grad = False
+        for layer in self.embed2audio.values():
+            for param in layer.parameters():
+                param.requires_grad = False
+
+        for layer in self.embed2visual.values():
+            for param in layer.parameters():
+                param.requires_grad = False
 
     def init_embedding(self, embedding):
         assert embedding.size()[-1] == self.embedding_dim
@@ -79,18 +85,15 @@ class AudioVisualGenerator(nn.Module):
         self.embedding_dim = self.embedding.size()[-1]
 
     def forward(self, embeddings):
-        #assert self.embedding is not None
-
-        #to_gen = self.embedding[idxes]
         to_gen = embeddings
 
         # from sentence embedding, generate mean and variance of
         # audio and visual features
         # since variance is positive, we exponentiate.
-        audio_mu = self.embed2audio_mu(to_gen)
-        audio_sigma = self.embed2audio_sigma(to_gen).exp()
+        audio_mu = self.embed2audio['mu'](to_gen)
+        audio_sigma = self.embed2audio['log_sigma'](to_gen).exp()
 
-        visual_mu = self.embed2visual_mu(to_gen)
-        visual_sigma = self.embed2visual_sigma(to_gen).exp()
+        visual_mu = self.embed2visual['mu'](to_gen)
+        visual_sigma = self.embed2visual['log_sigma'](to_gen).exp()
 
         return (audio_mu, audio_sigma), (visual_mu, visual_sigma)
